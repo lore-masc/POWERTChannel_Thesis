2b8dd3637c8ad7a99ce27e475b6eaf498712b07d
Lorenzo <lorenzo.masciullo@studenti.unitn.it>
2020-06-16 15:57:38 +0200

    Sink refactoring

--- POWERTSink/app/src/main/java/com/powert/sink/Task.java
+++ POWERTSink/app/src/main/java/com/powert/sink/Task.java
@@ -15,9 +15,9 @@ import androidx.annotation.RequiresApi;
 import java.util.ArrayList;
 
 public class Task extends AsyncTask<Void, String, Void> {
-    private static long wait;            // [ms]
+    private static long wait;                               // [ms]
     private static final long READ_CORES = 20;              // [ms]
-    private static float bit_threshold_start = 0.50f;
+    private static float bit_threshold;
     private static final float PEAK = 0.8f;
     private static final int BYTE = 8;
     private static final int PREAMBLE_SIZE = 5;
@@ -34,6 +34,7 @@ public class Task extends AsyncTask<Void, String, Void> {
     Task (LinearLayout ll, Context context) {
         this.ll = ll;
         this.context = context;
+        bit_threshold = 0.46f;
         wait = Long.parseLong(((EditText) ll.findViewById(R.id.editTextNumber)).getText().toString());
         average_size = Math.round(wait / READ_CORES);
 
@@ -43,7 +44,6 @@ public class Task extends AsyncTask<Void, String, Void> {
             rumor_workload.add(Utils.readCore(READ_CORES));
         overhead_workload = weightedAverageArray(rumor_workload);
         publishProgress("", "", "", String.valueOf(overhead_workload));
-//        bit_threshold_start = bit_threshold_start - overhead_workload;
     }
 
     public void setRecording(boolean recording) {
@@ -64,47 +64,48 @@ public class Task extends AsyncTask<Void, String, Void> {
         long start_range = System.currentTimeMillis();
         long consecutive_bits;
         ArrayList<Integer> bits = new ArrayList<>();
-        ArrayList<Float> workloads = new ArrayList<>();
+        ArrayList<Float> samples = new ArrayList<>();
 
         while (this.isRecording()) {
             Log.d("SINK", "" + bits);
-            float measured_workload;
+            float sampled_workload;
 
             // waiting first peak
             do {
-                measured_workload = Math.max(0, Utils.readCore(READ_CORES) - overhead_workload);
-                if (start && measured_workload >= PEAK) {
-                    Log.d("SINK", "MEASURED: " + measured_workload);
+                sampled_workload = Math.max(0, Utils.readCore(READ_CORES) - overhead_workload);
+                if (start && sampled_workload >= PEAK) {
+//                    Log.d("SINK", "MEASURED: " + sampled_workload);
                     start = false;
                     start_range = System.currentTimeMillis();
                 }
-            } while (start && measured_workload < PEAK);
-            workloads.add(measured_workload);
+            } while (start && sampled_workload < PEAK);
+            samples.add(sampled_workload);
 
             long last_measure = System.currentTimeMillis();
 
-            if (workloads.size() > average_size) {
-                workloads.remove(0);
-                float workload = weightedAverageArray(workloads);
-                publishProgress(mode, String.valueOf(workload), "", "");
+            if (samples.size() > average_size) {
+                samples.remove(0);
+                float actual_workload = weightedAverageArray(samples);
+
+                publishProgress(mode, String.valueOf(actual_workload), "", "");
 
                 // synchronization
-                if (workload >= bit_threshold_start && zero_bit) {
+                if (actual_workload >= bit_threshold && zero_bit) {
                     consecutive_bits = Math.max(1, Math.round((last_measure - start_range) / (wait * 1.0f)));
-                    Log.d("SINK", "Entrato nel 1 dopo " + (last_measure - start_range) + " ms = " + Math.round((last_measure - start_range) / (wait * 1.0f)));
+//                    Log.d("SINK", "Entrato nel 1 dopo " + (last_measure - start_range) + " ms = " + Math.round((last_measure - start_range) / (wait * 1.0f)));
                     start_range = System.currentTimeMillis();
                     if (consecutive_bits >= PREAMBLE_SIZE && synchronization) {
                         bits.clear();
                         mode = this.context.getResources().getString(R.string.mode2);
                         synchronization = false;
                         consecutive_bits -= PREAMBLE_SIZE;
-                        publishProgress(mode, String.valueOf(workload), "", "");
+                        publishProgress(mode, String.valueOf(actual_workload), "", "");
                     }
                     for (int i = 0; i < consecutive_bits; i++) bits.add(0);
                     zero_bit = false;
-                } else if (workload < bit_threshold_start && !zero_bit) {
+                } else if (actual_workload < bit_threshold && !zero_bit) {
                     consecutive_bits = Math.max(1, Math.round((last_measure - start_range) / (wait * 1.0f)));
-                    Log.d("SINK", "Entrato nello 0 dopo " + (last_measure - start_range) + " ms = " + Math.round((last_measure - start_range) / (wait * 1.0f)));
+//                    Log.d("SINK", "Entrato nello 0 dopo " + (last_measure - start_range) + " ms = " + Math.round((last_measure - start_range) / (wait * 1.0f)));
                     start_range = System.currentTimeMillis();
                     for (int i = 0; i < consecutive_bits; i++) bits.add(1);
                     zero_bit = true;
@@ -121,7 +122,7 @@ public class Task extends AsyncTask<Void, String, Void> {
 
                     for (int i = 0; i < BYTE; i++)
                         bits.remove(0);
-                    publishProgress(mode, String.valueOf(workload), String.valueOf(dec), "");
+                    publishProgress(mode, String.valueOf(actual_workload), String.valueOf(dec), "");
                 } else if (!synchronization && Math.round((last_measure - start_range)/(wait * 1.0f)) > BYTE+(BYTE-bits.size())) {
                     // corner case of remaining zero-bits
                     for (int i = 0; i < BYTE - bits.size(); i++)  bits.add(0);
@@ -150,9 +151,13 @@ public class Task extends AsyncTask<Void, String, Void> {
         TextView textView8 = ll.findViewById(R.id.textView8);
         TextView textView9 = ll.findViewById(R.id.textView9);
         TextView textView10 = ll.findViewById(R.id.textView10);
+        TextView textView13 = ll.findViewById(R.id.textView13);
 
         if (!values[3].equals(""))
             textView10.setText("Overhead workload: " + values[3]);
+
+        if (values.length == 5)
+            textView13.setText("Last evaluated threshold: " + values[4]);
 
         if (!values[2].equals(""))
             editText3.append(String.valueOf(Character.toChars(Integer.parseInt(values[2]))[0]));
@@ -183,6 +188,14 @@ public class Task extends AsyncTask<Void, String, Void> {
             sum += arr.get(i);
         }
         return  sum / arr.size();
+    }
+
+    public static float variance(ArrayList<Float> arr) {
+        float m = arithmeticAverageArray(arr);
+        float sommaScartiQuad = 0;
+        for(int i = 0; i < arr.size(); i++)
+            sommaScartiQuad += (arr.get(i) -m) * (arr.get(i) -m);
+        return sommaScartiQuad/arr.size();
     }
 
 }
